5a524992277b20c7734662984e71b6834aa0d469
Lúcio Brito <luciopereirabritoo@gmail.com>
2022-03-18 00:47:34 -0300

    adicionado footer e atualizado script

--- navbar/script.js
+++ navbar/script.js
@@ -1,6 +1,11 @@
 const menuToggle = document.querySelector('div.menu__toggle');
 const menuMobile = document.querySelector('div.menu__mobile');
+const headerSpace = document.querySelector('div#header__conteiner');
+let menu_active = true;
+
 
 menuMobile.addEventListener('click', () => {
     menuToggle.classList.toggle('on');
+    headerSpace.style.padding = menu_active ? '2rem 2rem 18rem 2rem' : '2rem';
+    menu_active = !menu_active;
 });
